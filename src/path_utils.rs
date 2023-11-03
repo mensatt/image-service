cdadfa59516ccf3703136850eb59ea4331258432
StealWonders <6114858+StealWonders@users.noreply.github.com>
2023-11-03 13:42:18 +0100

    Adds path explanations

--- src/path_utils.rs
+++ src/path_utils.rs
@@ -2,18 +2,22 @@ use std::path::PathBuf;
 
 use crate::constants::{CACHE_PATH, ORIGINAL_PATH, PENDING_PATH, UNAPPROVED_PATH};
 
+// Path of images that are not yet assigned to a review
 pub fn get_pending_path() -> PathBuf {
     return PENDING_PATH.iter().collect();
 }
 
+// Path where images of unapproved reviews are stored
 pub fn get_unapproved_path() -> PathBuf {
     return UNAPPROVED_PATH.iter().collect();
 }
 
+// Path where public servable images are stored
 pub fn get_original_path() -> PathBuf {
     return ORIGINAL_PATH.iter().collect();
 }
 
+// Path for image cache
 pub fn get_cache_path() -> PathBuf {
     return CACHE_PATH.iter().collect();
 }
